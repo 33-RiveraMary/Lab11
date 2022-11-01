f9f9286d24e79986641f99108ce663501e3f773f
33-RiveraMary <114891412+33-RiveraMary@users.noreply.github.com>
2022-11-01 10:35:26 +0800

    Adding unity tests
    - Test if game can restart
    - Test if laser can move upwards

--- Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
+++ Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -50,5 +50,30 @@ namespace Tests
             //3
             Assert.True(game.isGameOver);
         }
+
+
+
+        [UnityTest]
+        public IEnumerator NewGameRestartsGame()
+        {
+            //1
+            game.isGameOver = true;
+            game.NewGame();
+            //2
+            Assert.False(game.isGameOver);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator LaserMovesUp()
+        {
+            // 1
+            GameObject laser = game.GetShip().SpawnLaser();
+            // 2
+            float initialYPos = laser.transform.position.y;
+            yield return new WaitForSeconds(0.1f);
+            // 3
+            Assert.Greater(laser.transform.position.y, initialYPos);
+        }
     }
 }
