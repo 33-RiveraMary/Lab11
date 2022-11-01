0954dac0d602dc9ab639147b4c404556477074ec
33-RiveraMary <114891412+33-RiveraMary@users.noreply.github.com>
2022-11-01 10:37:01 +0800

    Adding unity tests
    - Test if laser can destroy asteroid

--- Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
+++ Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -75,5 +75,18 @@ namespace Tests
             // 3
             Assert.Greater(laser.transform.position.y, initialYPos);
         }
+
+        [UnityTest]
+        public IEnumerator LaserDestroysAsteroid()
+        {
+            // 1
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            asteroid.transform.position = Vector3.zero;
+            GameObject laser = game.GetShip().SpawnLaser();
+            laser.transform.position = Vector3.zero;
+            yield return new WaitForSeconds(0.1f);
+            // 2
+            UnityEngine.Assertions.Assert.IsNull(asteroid);
+        }
     }
 }
